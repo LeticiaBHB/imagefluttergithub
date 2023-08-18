e6c470c6e1b8f4a82c2a3b587bb694fba9294f3e
bowie <leticia.bini@gmail.com>
2023-08-17 22:08:35 -0300

    continua

--- lib/main.dart
+++ lib/main.dart
@@ -30,9 +30,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var numeroGerado = 0;
-  List  _imagens = [
+  final List <String> _imagens = [
+    'assets/1.jpeg'
+    'assets/2.jpeg'
+    'assets/3.jpeg'
+    'assets/4.jpeg'
+    'assets/5.jpeg'
+    'assets/6.jpeg'
+    'assets/7.jpeg'
+    'assets/8.jpeg'
 
-  ]
+  ];
 
  void _gerarnumeroAleatorio (){
   Random numeroAleatorio = Random();
